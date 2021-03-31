0664a444cf4d169dbae4f059f1e322e5cd1ce95a
Carlos Alonso <carlos.martinez@mataro.epiaedu.cat>
2021-03-31 11:03:13 +0200

    updated website

---
+++ index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>Inici</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="index.html">Inici</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="alert.html">Alertes</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="buttons.html">Botons </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="progressbar.html">Barra de progrés </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="spinners.html">Spinners </a>
+                </li>
+                <li class="nav-item">
+                    <a href="dropdowns.html" class="nav-link">Dropdowns </a>
+                </li>
+                <li class="nav-item">
+                    <a href="carousels.html" class="nav-link">Carousels</a>
+                </li>
+                <li class="nav-item">
+                    <a href="inputs.html" class="nav-link">Inputs </a>
+                </li>
+                <li class="nav-item">
+                    <a href="modals.html" class="nav-link">Modals </a>
+                </li>
+                <li class="nav-item">
+                    <a href="collapse.html" class="nav-link">Collapse </a>
+                </li>
+                <li class="nav-item">
+                    <a href="tooltips.html" class="nav-link">Tooltips </a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="container-fluid">
+        <div class="jumbotron">
+            <h1>DEMO BOOTSTRAP</h1>
+            <p>Aquest projecte l'hem dissenyat com a demo de Bootstrap <br />
+            
+        </div>
+    </div>
+    <div class="container fluid">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card" style="width:300px; height:300px;">
+                    <img id="img_roger" src="img/img_roge.png" alt="Card Image" class="car-img-top" style="width:300px; height:300px;">
+                    <div class="card-bod">
+                        <h4 class="card-title">John Smith</h4>
+                        <p class="card-text">Person 1.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card" style="width:300px; height:300px;">
+                    <img id="img_cris" src="img/img_cristobal.png" alt="Card Image" class="car-img-top" style="width:300px; height:300px;">
+                    <div class="card-bod">
+                        <h4 class="card-title">Bill Blake</h4>
+                        <p class="card-text">Person 2.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card" style="width:300px; height:300px;">
+                    <img id="img_cris" src="img/img_adri.png" alt="Card Image" class="car-img-top" style="width:300px; height:300px;">
+                    <div class="card-bod">
+                        <h4 class="card-title">Chris Childs</h4>
+                        <p class="card-text">Person 3.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>    
+    <script src="https://code.jquery.com/jquery-4.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
